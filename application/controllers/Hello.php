45750440510d7b4fc050096c93ccc1ec92778d63
irfanakbari <irfanakbarihabibi18@gmail.com>
2022-03-22 15:52:40 +0700

    first commit

---
+++ application/controllers/Hello.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Hello extends CI_Controller
+{
+
+
+    public function aku()
+    {
+        echo "Hello World!";
+    }
+}
